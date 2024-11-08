ef4162b66dd7960376c61aee6bb0c773afeaaba5
JB Stepan <29986350+JBStepan@users.noreply.github.com>
2024-11-07 23:11:31 -0600

    Created URL shortener

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		console.log(url.pathname)
+		if(url.pathname == "/") {
+			return new Response("JB Stepan URL Shortener")
+		} else if(url.pathname == "/create" && request.method == "POST") {
+			const auth = request.headers.get("Auth")
+
+			if(auth == null || auth != env.APIKEY) {
+				return new Response("Unauthorized", { status: 401 })
+			}
+
+			return new Response("Good")
+		} else {
+			const url = new URL(request.url);
+			const slug = url.pathname.slice(1);
+
+			const cache = caches.default;
+   			let cacheKey = new Request(request.url);
+
+			let cachedResponse = await cache.match(cacheKey);
+			if (cachedResponse) {
+				return cachedResponse; 
+			}
+
+			const redirectUrl = await env.JSHORT.get(slug)
+			if(!redirectUrl) {
+				return new Response("URL not found", { status: 404 })
+			}
+			
+			let res = Response.redirect(redirectUrl, 301)
+			await cache.put(cacheKey, res.clone());
+
+			return res
+		}
 	},
 } satisfies ExportedHandler<Env>;
